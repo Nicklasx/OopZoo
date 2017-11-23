54a714478483345c6a9206adc566eb9c546037b2
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2017-11-23 12:42:49 +0100

    tilføjede class species

---
+++ OopZoo/OopZoo/Species.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopZoo
+{
+	class Species
+	{
+		private string species;
+
+		public Species(string species)
+		{
+			this.species = species;
+		}
+
+		public string Describe()
+		{
+			return "Dette dyr er en " + species;
+		}
+
+		public string Animal
+		{
+			get { return species; }
+			set { species = value; }
+		}
+
+	}
+}
