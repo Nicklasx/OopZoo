4b3c25ebe23c9e04bd953d7ba3e1bc6f135be5bc
Nicklas Pihl Mathiesen <nick5666@edu.mercantec.dk>
2017-11-23 09:13:56 +0100

    ændrede i koden

--- OopZoo/OopZoo/Program.cs
+++ OopZoo/OopZoo/Program.cs
@@ -10,7 +10,6 @@ namespace OopZoo
     {
         static void Main(string[] args)
         {
-
             Console.ReadKey();
         }
     }
--- OopZoo/OopZoo/Zoo.cs
+++ OopZoo/OopZoo/Zoo.cs
@@ -2,11 +2,5 @@
 
 class Zoo
 {
-    protected string zooName = "Zoo Tilt";
-
-    public string zoo()
-    {
-        Console.WriteLine("velkommen til" + zooName);
-        return "0";
-    }
+    
 }
